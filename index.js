21d184f262b822709a54dd236adbabb0f836914d
Jerome Bolinas <bolinasjerome@itis-molinari.eu>
2024-12-15 18:50:20 +0100

    pagina funzionante

---
+++ index.js
@@ -0,0 +1,3 @@
+import { createNavigator } from "./navigator.js";
+
+const navigator = createNavigator(document.querySelector("#container"));
---
+++ navigator.js
@@ -0,0 +1,24 @@
+const hide = (elements) => {
+    elements.forEach((element) => {
+       element.classList.add("hidden");
+       element.classList.remove("visible");
+    });
+ }
+ 
+ const show = (element) => {
+    element.classList.add("visible");
+    element.classList.remove("hidden");   
+ }
+ 
+ export const createNavigator = (parentElement) => {
+    const pages = Array.from(parentElement.querySelectorAll(".page"));
+    const render = () => {
+       const url = new URL(document.location.href);
+       const pageName = url.hash.replace("#", "");
+       const selected = pages.filter((page) => page.id === pageName)[0] || pages[0];
+       hide(pages);
+       show(selected);
+    }
+    window.addEventListener('popstate', render); 
+    render();   
+ }
